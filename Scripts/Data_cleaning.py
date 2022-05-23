f851451e5408a6290c9ed5c332b772538e2f6671
Kevin <shyakakevin1@gmail.com>
2022-05-23 19:12:46 +0200

    file modified

---
+++ Scripts/Data_cleaning.py
@@ -0,0 +1,23 @@
+import pandas as pd 
+import numpy as np 
+
+def load_data(filepath):
+    df = pd.read_csv(filepath)
+    return df 
+
+class clean_data():
+    def __init__(self,df):
+        self.df = df
+    
+    def descriptive_statistics(self,info=False,shape=False,describe=False,missing_values=False):
+        information=None
+        if info:
+            information = self.df.info()
+        elif shape:
+            information = self.df.shape
+        elif describe:
+            information = self.df.describe()
+        elif missing_values:
+            information = self.df.isna().sum()
+        
+        return information
