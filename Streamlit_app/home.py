1b442d0267eac720b404afdec4a640f053c9999c
Kevin <shyakakevin1@gmail.com>
2022-05-30 09:44:27 +0200

    add requirement,txt

--- Streamlit_app/home.py
+++ Streamlit_app/home.py
@@ -1,9 +1,12 @@
 import streamlit as st
 
+import pandas as pd 
+import numpy as np 
 
-with st.spinner("Loading Home ..."):
-    st.markdown("<p style='padding:2rem;text-align:center; background-color:#3761B5;color:#FFFFFF;font-size:2rem;border-  radius:0.8rem;'>Rossmann Pharmaceuticals</p>", unsafe_allow_html=True)
+train_data = pd.read_csv('./data/train-data.csv')
+
+st.title('Pharmaceutical Sales')
+if set.checkbox('Train data'):
+    st.subheader('Training Dataset')
+    st.write(train_data)
     
-    st.markdown("<p style='font-size:1.8rem'>Dirk Rossmann GmbH (usual: Rossmann) is one of the largest drug store chains in Europe with around 56,200 employees and more than 4000 stores across Europe.</p>", unsafe_allow_html=True)
-
-    st.markdown("<p style='font-size:1.8rem'>This app is an end-to-end solution that allows the Rosemann pharmaceutical company to see sales forecasts for its stores six weeks in advance, as well as expected trends.</p>",unsafe_allow_html=True)
