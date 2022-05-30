fc02cb41158d8a783be5ac302b664a78a15dcb10
Kevin <shyakakevin1@gmail.com>
2022-05-30 08:55:22 +0200

    streamlit app

---
+++ Streamlit_app/home.py
@@ -0,0 +1,9 @@
+mport streamlit as st
+
+
+with st.spinner("Loading Home ..."):
+    st.markdown("<p style='padding:2rem;text-align:center; background-color:#3761B5;color:#FFFFFF;font-size:2rem;border-  radius:0.8rem;'>Rossmann Pharmaceuticals</p>", unsafe_allow_html=True)
+    
+    st.markdown("<p style='font-size:1.8rem'>Dirk Rossmann GmbH (usual: Rossmann) is one of the largest drug store chains in Europe with around 56,200 employees and more than 4000 stores across Europe.</p>", unsafe_allow_html=True)
+
+    st.markdown("<p style='font-size:1.8rem'>This app is an end-to-end solution that allows the Rosemann pharmaceutical company to see sales forecasts for its stores six weeks in advance, as well as expected trends.</p>",unsafe_allow_html=True)
