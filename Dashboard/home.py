1fb545feaf761e18ff491f003fe2396e9d66722f
Kevin <shyakakevin1@gmail.com>
2022-06-06 12:39:55 +0200

    Dashboard with streamlit

--- Dashboard/home.py
+++ Dashboard/home.py
@@ -3,7 +3,7 @@ import streamlit as st
 def app():
     with st.spinner("Loading Home ..."):
         
-        st.markdown("<p style='padding:2rem;text-align:center; background-color:#3761B5;color:#FFFFFF;font-size:2rem;border-radius:0.8rem;'>Rossmann Pharmaceuticals</p>", unsafe_allow_html=True)
+        st.markdown("<p style='padding:2rem;text-align:center; background-color:#3761B5;color:#FFFFFF;font-size:2rem;border-radius:0.5rem;'>Rossmann Pharmaceuticals Sales Forecasting</p>", unsafe_allow_html=False)
     
         st.markdown("<p style='font-size:1.8rem'>With about 56,200 employees and over 4000 locations across Europe, Dirk Rossmann GmbH (commonly known as Rossmann) is one of Europe's major drugstore companies.</p>", unsafe_allow_html=True)
 
