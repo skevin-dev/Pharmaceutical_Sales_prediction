d6388d9e7d56f9c3a19b3c847359309d8919ec06
Kevin <shyakakevin1@gmail.com>
2022-06-06 13:29:38 +0200

    finishing dashboard with streamlit

--- Dashboard/display_traindataset.py
+++ Dashboard/display_traindataset.py
@@ -7,10 +7,12 @@ def app():
     with st.spinner("Loading Data ..."):
         st.markdown("<p style='padding:2rem;text-align:center; background-color:#3761B5;color:#FFFFFF;font-size:2rem;border-radius:0.8rem;'>Data display page</p>", unsafe_allow_html=True)
      
-        st.markdown("<p style='font-size:1.8rem'>The report includes sales statistics from 1,115 Rossmann outlets over the years.</p>", unsafe_allow_html=True)
+        st.markdown("<p style='font-size:1rem'>The report includes sales statistics from 1,115 Rossmann outlets over the years.</p>", unsafe_allow_html=True)
 
-        st.markdown("<p style='font-size:1.8rem'>This challenge's data and feature description can be found at https://www.kaggle.com/c/rossmann-store-sales.</p>",unsafe_allow_html=True)
- 
+        st.markdown("<p style='font-size:1rem'>This challenge's data and feature description can be found at https://www.kaggle.com/c/rossmann-store-sales.</p>",unsafe_allow_html=True)
+        
+        
+        @st.cache
         st.header('Training Data')
         train = pd.read_csv('./data/train.csv',low_memory=False)
         st.write(train, width=1200)
