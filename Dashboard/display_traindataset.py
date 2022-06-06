f42f93496c30cebe60fd320ce9dc01a9ca33cfd3
Kevin <shyakakevin1@gmail.com>
2022-06-06 13:24:36 +0200

    finishing dashboard with streamlit

--- Dashboard/display_traindataset.py
+++ Dashboard/display_traindataset.py
@@ -11,6 +11,10 @@ def app():
 
         st.markdown("<p style='font-size:1.8rem'>This challenge's data and feature description can be found at https://www.kaggle.com/c/rossmann-store-sales.</p>",unsafe_allow_html=True)
  
-
+        st.header('Training Data')
         train = pd.read_csv('./data/train.csv',low_memory=False)
         st.write(train, width=1200)
+        
+        st.header('Testing Data')
+        test = pd.read_csv('./data/test.csv')
+        st.write(test)
